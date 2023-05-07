b65e2a63a824b6a9b7336e6e94f01f7a066a594a
IAmGroots <hadiseptian.crb@gmail.com>
2023-05-07 17:01:55 +0800

    T5

---
+++ 2109106058_Muhammad Hadiseptian Nursukamto_T5/2109106058_Muhammad Hadiseptian Nursukamto_Tugas2_Pak_Reza/App.Designer.vb
@@ -0,0 +1,272 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class App
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(App))
+        Me.txtIdSertifikat = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtNama = New System.Windows.Forms.TextBox()
+        Me.txtNIK = New System.Windows.Forms.TextBox()
+        Me.dtpTglVaksin = New System.Windows.Forms.DateTimePicker()
+        Me.dtpTtl = New System.Windows.Forms.DateTimePicker()
+        Me.cmbJenis = New System.Windows.Forms.ComboBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.lblTanggal = New System.Windows.Forms.Label()
+        Me.lblJam = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtIdSertifikat
+        '
+        Me.txtIdSertifikat.BackColor = System.Drawing.Color.White
+        Me.txtIdSertifikat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtIdSertifikat.Enabled = False
+        Me.txtIdSertifikat.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtIdSertifikat.Location = New System.Drawing.Point(154, 34)
+        Me.txtIdSertifikat.Name = "txtIdSertifikat"
+        Me.txtIdSertifikat.Size = New System.Drawing.Size(200, 32)
+        Me.txtIdSertifikat.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(102, 114)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "NIK"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(24, 153)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 23)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Tanggal Lahir"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 189)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(136, 23)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Tanggal Vaksin"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(35, 37)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 23)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "ID Sertifikat"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(14, 75)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(134, 23)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Nama Lengkap"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(36, 226)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(112, 23)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Jenis Vaksin"
+        '
+        'txtNama
+        '
+        Me.txtNama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtNama.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNama.Location = New System.Drawing.Point(154, 72)
+        Me.txtNama.Name = "txtNama"
+        Me.txtNama.Size = New System.Drawing.Size(200, 32)
+        Me.txtNama.TabIndex = 7
+        '
+        'txtNIK
+        '
+        Me.txtNIK.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtNIK.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNIK.Location = New System.Drawing.Point(154, 110)
+        Me.txtNIK.Name = "txtNIK"
+        Me.txtNIK.Size = New System.Drawing.Size(200, 32)
+        Me.txtNIK.TabIndex = 8
+        '
+        'dtpTglVaksin
+        '
+        Me.dtpTglVaksin.Location = New System.Drawing.Point(154, 190)
+        Me.dtpTglVaksin.Name = "dtpTglVaksin"
+        Me.dtpTglVaksin.Size = New System.Drawing.Size(200, 20)
+        Me.dtpTglVaksin.TabIndex = 12
+        '
+        'dtpTtl
+        '
+        Me.dtpTtl.Location = New System.Drawing.Point(154, 155)
+        Me.dtpTtl.Name = "dtpTtl"
+        Me.dtpTtl.Size = New System.Drawing.Size(200, 20)
+        Me.dtpTtl.TabIndex = 13
+        '
+        'cmbJenis
+        '
+        Me.cmbJenis.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbJenis.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbJenis.FormattingEnabled = True
+        Me.cmbJenis.Items.AddRange(New Object() {"Sinovac", "AstraZeneca", "Sinopharm", "Moderna", "Pfizer", "Novavax"})
+        Me.cmbJenis.Location = New System.Drawing.Point(154, 223)
+        Me.cmbJenis.Name = "cmbJenis"
+        Me.cmbJenis.Size = New System.Drawing.Size(200, 31)
+        Me.cmbJenis.TabIndex = 15
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.White
+        Me.btnSave.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSave.FlatAppearance.BorderSize = 0
+        Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSave.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(257, 260)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(97, 32)
+        Me.btnSave.TabIndex = 16
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.White
+        Me.btnReset.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnReset.FlatAppearance.BorderSize = 0
+        Me.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnReset.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(154, 260)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(97, 32)
+        Me.btnReset.TabIndex = 17
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'lblTanggal
+        '
+        Me.lblTanggal.AutoSize = True
+        Me.lblTanggal.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTanggal.Location = New System.Drawing.Point(5, 5)
+        Me.lblTanggal.Name = "lblTanggal"
+        Me.lblTanggal.Size = New System.Drawing.Size(143, 19)
+        Me.lblTanggal.TabIndex = 19
+        Me.lblTanggal.Text = "Monday, 1 May 2023"
+        '
+        'lblJam
+        '
+        Me.lblJam.AutoSize = True
+        Me.lblJam.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblJam.Location = New System.Drawing.Point(291, 5)
+        Me.lblJam.Name = "lblJam"
+        Me.lblJam.Size = New System.Drawing.Size(63, 19)
+        Me.lblJam.TabIndex = 20
+        Me.lblJam.Text = "07:07:07"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'App
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.ClientSize = New System.Drawing.Size(368, 304)
+        Me.Controls.Add(Me.lblJam)
+        Me.Controls.Add(Me.lblTanggal)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.cmbJenis)
+        Me.Controls.Add(Me.dtpTtl)
+        Me.Controls.Add(Me.dtpTglVaksin)
+        Me.Controls.Add(Me.txtNIK)
+        Me.Controls.Add(Me.txtNama)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtIdSertifikat)
+        Me.Name = "App"
+        Me.Text = "2109106058 - Pendataan Vaksin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtIdSertifikat As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents txtNIK As TextBox
+    Friend WithEvents dtpTglVaksin As DateTimePicker
+    Friend WithEvents dtpTtl As DateTimePicker
+    Friend WithEvents cmbJenis As ComboBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents lblTanggal As Label
+    Friend WithEvents lblJam As Label
+    Friend WithEvents Timer1 As Timer
+End Class
